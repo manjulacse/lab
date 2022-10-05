2652476228300ddf2292f72283006d109ff7962c
manjulacse <113112600+manjulacse@users.noreply.github.com>
2022-10-05 17:25:28 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,10 @@
+class Static {
+  static String name= " manjula";
+  public static void main( String args[])   {
+       System.out.println ( name);
+
+     public static void main() {
+       System.outprintln( "   hiiii have a good day " );
+     }
+}
+}
