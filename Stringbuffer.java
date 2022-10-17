515f0752477cb37c54f76058f42b3de6cb2561c2
manjulacse <113112600+manjulacse@users.noreply.github.com>
2022-10-17 08:44:18 +0530

    Add files via upload

---
+++ Stringbuffer.java
@@ -0,0 +1,40 @@
+package javapackage;
+
+public class Stringbuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	StringBuffer s1= new StringBuffer("sairam");
+	System.out.println(s1);
+	System.out.println(s1.reverse());
+	System.out.println(s1.append("ram"));
+	System.out.println(s1.insert(0, "sai"));
+	System.out.println(s1);
+	System.out.println(s1.delete(3, 5));
+	System.out.println(s1.length());
+	System.out.println(s1.equals("sairam"));
+	
+	}
+
+}
+
+
+public class Stringbuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	StringBuffer s1= new StringBuffer("sairam");
+	System.out.println(s1);
+	System.out.println(s1.reverse());
+	System.out.println(s1.append("ram"));
+	System.out.println(s1.insert(0, "sai"));
+	System.out.println(s1);
+	System.out.println(s1.delete(3, 5));
+	System.out.println(s1.length());
+	System.out.println(s1.equals("sairam"));
+	
+	}
+
+}
