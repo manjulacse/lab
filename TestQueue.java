03e26274492daf54a627b567f4a26a05afad0dce
manjulacse <113112600+manjulacse@users.noreply.github.com>
2022-10-31 08:29:06 +0530

    Add files via upload

---
+++ TestQueue.java
@@ -0,0 +1,17 @@
+package javapackage;
+import java.util.*;
+public class TestQueue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     PriorityQueue<String> queue= new PriorityQueue<String>();
+     queue.add("amit");
+     queue.add("vijay");
+     queue.add("haii");
+     queue.remove();
+     queue.poll();
+     
+     
+	}
+
+}
